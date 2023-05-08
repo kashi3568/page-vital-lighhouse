61c84fdfa692e8422cc52783db0e1173777718fd
Kashish <kashish.insan@infoedge.com>
2023-05-08 18:09:58 +0530

    update

--- app.js
+++ app.js
@@ -8,24 +8,25 @@ import pageVitalsRoute from "./routes/PageVitalRoutes.js";
 import axios from 'axios';
 
 import pkg from 'mongoose';
+import ServerlessHttp from "serverless-http";
 const { connect, connection } = pkg;
 //configure mongoose
-connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true
+//});
 
-const db = connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Connected to MongoDB");
-});
+//const db = connection;
+//db.on("error", console.error.bind(console, "connection error:"));
+//db.once("open", function () {
+//  console.log("Connected to MongoDB");
+//});
  
 app.listen(3004, () => {
   console.log("Server is running on port 3004");
 });
 
-schedule('0 3 * * *', () => {
+schedule('* * * * *', () => {
   console.log('running a task every minute');
   // axios.get('http://localhost:3004/api/pageVitals/generateData').then((res)=> {
   //   console.log(res)
