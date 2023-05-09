ed198685d892d739ce554b0c38af4d60e606651d
Kashish <kashish.insan@infoedge.com>
2023-05-09 13:08:10 +0530

    commit

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ const { connect, connection } = pkg;
 //db.once("open", function () {
 //  console.log("Connected to MongoDB");
 //});
- 
-app.listen(3004, () => {
-  console.log("Server is running on port 3004");
+
+app.get('*', (req, res) => {
+  res.send('Hello, World!');
 });
 
-schedule('* * * * *', () => {
-  console.log('running a task every minute');
-  // axios.get('http://localhost:3004/api/pageVitals/generateData').then((res)=> {
-  //   console.log(res)
-  // })
-  console.log('running a task every minute');
-});
+// schedule('* * * * *', () => {
+//   console.log('running a task every minute');
+//   // axios.get('http://localhost:3004/api/pageVitals/generateData').then((res)=> {
+//   //   console.log(res)
+//   // })
+//   console.log('running a task every minute');
+// });
 app.use("/api/urls", urlRequest);
 app.use("/api/pageVitals", pageVitalsRoute);
 export default app;
